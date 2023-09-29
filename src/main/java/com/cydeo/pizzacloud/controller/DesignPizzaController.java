dc6fc780e557c4640a6fc398e93ae64505dffd1f
fevzi <fevzifurkan2012@gmail.com>
2023-09-29 16:48:25 -0300

    .

--- src/main/java/com/cydeo/pizzacloud/controller/DesignPizzaController.java
+++ src/main/java/com/cydeo/pizzacloud/controller/DesignPizzaController.java
@@ -3,24 +3,33 @@ package com.cydeo.pizzacloud.controller;
 import com.cydeo.pizzacloud.bootstrap.DataGenerator;
 import com.cydeo.pizzacloud.model.Pizza;
 import com.cydeo.pizzacloud.repository.PizzaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.UUID;
 
 @Controller
-
+@RequestMapping("/design")
+@ComponentScan
 public class DesignPizzaController {
 
+    @Autowired
     private PizzaRepository pizzaRepository;
 
-    @PostMapping
+
+    @GetMapping
     public String showDesignForm(Model model) {
 
         model.addAttribute("cheeses", DataGenerator.cheeseTypeList);
         model.addAttribute("sauces", DataGenerator.sauceTypeList);
         model.addAttribute("toppings", DataGenerator.toppingTypeList);
+        model.addAttribute("pizza", new Pizza());
 
         return "/design";
 
--- src/main/java/com/cydeo/pizzacloud/controller/OrderController.java
+++ src/main/java/com/cydeo/pizzacloud/controller/OrderController.java
@@ -3,14 +3,18 @@ package com.cydeo.pizzacloud.controller;
 import com.cydeo.pizzacloud.model.Pizza;
 import com.cydeo.pizzacloud.model.PizzaOrder;
 import com.cydeo.pizzacloud.repository.PizzaRepository;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.SpringServletContainerInitializer;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @Controller
 @RequestMapping("/orders")
+@ComponentScan
 public class OrderController {
 
     private final PizzaRepository pizzaRepository;
@@ -20,7 +24,7 @@ public class OrderController {
     }
 
     @GetMapping("/current")
-    public String orderForm(UUID pizzaId, Model model) {
+    public String orderForm(@RequestParam UUID pizzaId, Model model) {
 
         PizzaOrder pizzaOrder = new PizzaOrder();
 
@@ -28,14 +32,16 @@ public class OrderController {
         pizzaOrder.setPizza(getPizza(pizzaId));
 
         model.addAttribute("pizzaOrder", pizzaOrder);
+        model.addAttribute("pizzaId", pizzaId);
 
         return "/orderForm";
     }
 
     @PostMapping("/{pizzaId}")
-    public String processOrder(UUID pizzaId, PizzaOrder pizzaOrder) {
-
+    public String processOrder(@PathVariable UUID pizzaId, PizzaOrder pizzaOrder) {
+        System.out.println(pizzaId);
         // Save the order
+        pizzaRepository.createPizza(pizzaOrder.getPizza());
 
         pizzaOrder.setPizza(getPizza(pizzaId));
         return "redirect:/home";
@@ -44,6 +50,12 @@ public class OrderController {
     //TODO
     private Pizza getPizza(UUID pizzaId) {
         // Get the pizza from repository based on it's id
+        List<Pizza> pizzaList = pizzaRepository.readAll();
+        for (Pizza pizza : pizzaList) {
+            if (pizza.getId().equals(pizzaId)) {
+                return pizza;
+            }
+        }
         return new Pizza();
     }
 
